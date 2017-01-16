d299e1e3a1bd2c105b132177a22e9cbd9def622e
Alicia Boya García <ntrrgc@gmail.com>
2017-01-16 01:22:47 +0100

    More JSObject tests

--- app/src/androidTest/java/me/ntrrgc/jsvm/JSObjectTests.java
+++ app/src/androidTest/java/me/ntrrgc/jsvm/JSObjectTests.java
@@ -64,4 +64,46 @@ public class JSObjectTests {
         assertTrue(obj.get("y").isUndefined());
         assertTrue(obj.get(0).isUndefined());
     }
+
+    @Test
+    public void testWriteNestedObjects() throws Exception {
+        JSObject nodeA = jsvm.evaluateScript("({value: 0, next: null})").asObject();
+        assertNotNull(nodeA);
+        JSObject nodeB = jsvm.evaluateScript("({value: 1, next: null})").asObject();
+        assertNotNull(nodeB);
+
+        nodeA.set("next", JSValue.anObject(nodeB));
+        assertEquals(1, nodeA.get("next").asObject().get("value").asInt().intValue());
+
+        nodeB.set("value", JSValue.aNumber(2));
+        assertEquals(2, nodeA.get("next").asObject().get("value").asInt().intValue());
+    }
+
+    @Test
+    public void crossingVMsThrows() throws Exception {
+        JSObject obj = jsvm.evaluateScript("({})").asObject();
+        assertNotNull(obj);
+
+        JSVM anotherVM = new JSVM();
+        JSObject foreignThingy = anotherVM.evaluateScript("({})").asObject();
+        assertNotNull(foreignThingy);
+
+        boolean caught = false;
+        try {
+            obj.set(0, JSValue.anObject(foreignThingy));
+        } catch (AttemptedToUseObjectFromOtherVM err) {
+            assertEquals(err.usedObject, foreignThingy);
+            assertEquals(err.usedObjectVM, anotherVM);
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    @Test
+    public void functionsAreObjects() throws Exception {
+        JSObject fun = jsvm.evaluateScript("(function hello() { return 'Hi'; })").asObject();
+        assertNotNull(fun);
+
+        assertEquals("hello", fun.get("name").asString());
+    }
 }
