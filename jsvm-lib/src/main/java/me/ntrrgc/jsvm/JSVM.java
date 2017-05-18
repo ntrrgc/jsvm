df8602e3078669968d2db670125bc5da9c64f477
Alicia Boya García <ntrrgc@gmail.com>
2017-05-19 01:02:59 +0200

    Change library name and find JNI package when building not for Android

--- jsvm-lib/src/main/java/me/ntrrgc/jsvm/JSVM.java
+++ jsvm-lib/src/main/java/me/ntrrgc/jsvm/JSVM.java
@@ -19,7 +19,7 @@ public class JSVM {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("jsvm");
         initializeLibrary();
     }
 
