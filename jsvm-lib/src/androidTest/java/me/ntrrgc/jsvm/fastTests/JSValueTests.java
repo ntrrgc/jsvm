a5faedab0bf65ae350f00579c9a8f675c7ee3f98
Alicia Boya García <ntrrgc@gmail.com>
2017-05-20 14:32:36 +0200

    Fix JUnit deprecation warning

--- jsvm-lib/src/androidTest/java/me/ntrrgc/jsvm/fastTests/JSValueTests.java
+++ jsvm-lib/src/androidTest/java/me/ntrrgc/jsvm/fastTests/JSValueTests.java
@@ -7,11 +7,11 @@ import org.junit.Test;
 import me.ntrrgc.jsvm.JSVM;
 import me.ntrrgc.jsvm.JSValue;
 
-import static junit.framework.Assert.assertSame;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 public class JSValueTests {
