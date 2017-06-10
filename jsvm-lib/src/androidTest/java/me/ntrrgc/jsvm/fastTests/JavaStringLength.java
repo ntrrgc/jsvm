e42094c5f88e021b3a4798d9e07fc666016aa0c0
Alicia Boya García <ntrrgc@gmail.com>
2017-06-10 20:08:32 +0200

    Test Java strings are compiled correctly

---
+++ jsvm-lib/src/androidTest/java/me/ntrrgc/jsvm/fastTests/JavaStringLength.java
@@ -0,0 +1,48 @@
+package me.ntrrgc.jsvm.fastTests;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test some assumptions on the behavior of Java strings in the JVM
+ * for code compiled in this project.
+ *
+ * If this test suite fails, it probably has been compiled with the
+ * wrong character encoding.
+ */
+public class JavaStringLength {
+
+    @Test
+    public void testAscii() throws Exception {
+        assertEquals(1, "a".length());
+    }
+
+    @Test
+    public void testAcute() throws Exception {
+        assertEquals(1, "á".length());
+    }
+
+    @Test
+    public void testSnowman() throws Exception {
+        assertEquals(1, "☃".length());
+    }
+
+    @Test
+    public void testGrinningCat() throws Exception {
+        assertEquals(2, "\uD83D\uDE3A".length());
+    }
+
+    @Test
+    public void testAcuteCodePoint() throws Exception {
+        String string = "á";
+        int[] actualCodePoints = new int[string.length()];
+        for (int i = 0; i < string.length(); i++) {
+            actualCodePoints[i] = string.codePointAt(i);
+        }
+        assertEquals("[225]", Arrays.toString(actualCodePoints));
+    }
+
+}
